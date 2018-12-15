7d813715f4f1f9e2689f86fa3eee5a51b6261e9c
Łukasz Jagliński <ljaglinski@o2.pl>
2018-12-15 21:12:02 +0100

    Define user interface variables

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Test');
+//Define user interface variable
+const form = document.querySelector('#item-form');
+const shoppingList = document.querySelector('.collection');
+const clearButton = document.querySelector('clear-items');
+const filter = document.querySelector('#filter');
+const itemInput = document.querySelector('#item');
