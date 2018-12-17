2b943c02950807d71b95488269bfb4584ecfd6fa
Łukasz Jagliński <ljaglinski@o2.pl>
2018-12-17 21:17:38 +0100

    Add function addItem which add new item to the shopping list

--- app.js
+++ app.js
@@ -10,3 +10,31 @@ loadEventListeners();
 function loadEventListeners() {
     form.addEventListener('submit', addItem);
 }
+
+function addItem(e) {
+    if (itemInput.value === '') {
+        alert('Add an item');
+    } else {
+        //create li element 
+        const li = document.createElement('li');
+        //add class
+        li.className = 'collection-item';
+        //create text node and append to created li
+        li.appendChild(document.createTextNode(itemInput.value));
+        //create new link element
+        const link = document.createElement('a');
+        //add class to created link
+        link.className = 'delete-item secondary-content';
+        //add icon
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //append the link to li
+        li.appendChild(link);
+        //append li to ul
+        shoppingList.appendChild(li);
+
+        //clear input
+        itemInput.value = '';
+
+        e.preventDefault();
+    }
+}
